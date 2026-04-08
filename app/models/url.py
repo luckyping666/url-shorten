1669b89cf27135fe57e413f105cf347555e87239
Roman Kuklin <kuklin.prog@gmail.com>
2026-04-08 17:34:58 +0800

    feat: add model url

---
+++ app/models/url.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, DateTime, func
+from core.database import Base
+
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    short_id: Mapped[str] = mapped_column(String(10), unique=True, index=True, nullable=False)
+    target_url: Mapped[str] = mapped_column(String, nullable=False)
+    clicks: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
